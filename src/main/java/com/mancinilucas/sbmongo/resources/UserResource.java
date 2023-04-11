d9287cebe761979b072cbc6ca7145bef32d98fde
mancinilucas <mancini.lucasg@gmail.com>
2023-04-11 18:56:18 -0300

    DTO pattern for Users

--- src/main/java/com/mancinilucas/sbmongo/resources/UserResource.java
+++ src/main/java/com/mancinilucas/sbmongo/resources/UserResource.java
@@ -1,17 +1,17 @@
 package com.mancinilucas.sbmongo.resources;
 
 import com.mancinilucas.sbmongo.domain.User;
+import com.mancinilucas.sbmongo.dto.UserDTO;
 import com.mancinilucas.sbmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,8 +21,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
