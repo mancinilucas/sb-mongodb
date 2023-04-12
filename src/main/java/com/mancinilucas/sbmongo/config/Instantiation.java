5331049af9bf5b2e8e2959b4dcf771b0dc05e758
mancinilucas <mancini.lucasg@gmail.com>
2023-04-12 10:56:50 -0300

    Referencing user posts: DBRef

--- src/main/java/com/mancinilucas/sbmongo/config/Instantiation.java
+++ src/main/java/com/mancinilucas/sbmongo/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, sdf.parse("10/04/2023"), "Partiu Capelinha", "Vou ali visitar Aline!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(p1,p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1,p2));
+        userRepository.save(maria);
     }
 }
