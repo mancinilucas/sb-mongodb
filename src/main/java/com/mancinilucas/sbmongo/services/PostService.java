e8a9395a74269015517851ce77a709c599a2c881
mancinilucas <mancini.lucasg@gmail.com>
2023-04-12 11:09:24 -0300

    Get Posts by Id impl

---
+++ src/main/java/com/mancinilucas/sbmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mancinilucas.sbmongo.services;
+
+import com.mancinilucas.sbmongo.domain.Post;
+import com.mancinilucas.sbmongo.repository.PostRepository;
+import com.mancinilucas.sbmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
